36cbb9177b0a162c154045f1dea5f5ff2a14312a
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-05-13 23:43:18 +0300

    LinkedHashMap put function

---
+++ linked_hash_map_put.c
@@ -0,0 +1,47 @@
+#include "linked_hash_map_put.h"
+
+void put(struct LinkedHashMap *map, int key, int value) {
+    struct Entry *elem = (struct Entry *) malloc(sizeof(*elem));
+    if (elem == NULL) {
+        printf("Error #1: Memory allocation failure while adding new list element");
+        exit(1);
+    }
+    elem->key = key;
+    elem->value = value;
+
+    elem->before = NULL;
+    elem->after = NULL;
+    elem->next = NULL;
+
+    int buckets = sizeof(map->table) / sizeof(map->table[0]);
+    int h = hash(key);
+    elem->hash = h;
+    int index = indexFor(h, buckets);
+
+    struct Entry *bucket = map->table[index];
+    if (bucket == NULL) {
+        map->table[index] = elem;
+    } else {
+        while (bucket != NULL) {
+            if (bucket->hash == h && bucket->key == key) {
+                bucket->value = value;
+                return;
+            }
+            if (bucket->next == NULL) {
+                bucket->next = elem;
+                break;
+            } else {
+                bucket = bucket->next;
+            }
+        }
+    }
+
+    if (map->head == NULL) {
+        map->head = elem;
+    } else {
+        map->tail->after = elem;
+        elem->before = map->tail;
+    }
+
+    map->tail = elem;
+}
